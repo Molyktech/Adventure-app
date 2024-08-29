bfd5d869c953d957dec3826d8daab6db92419b05
Adebayo Modupe Aisha <modupeadebayo001@gmail.com>
2024-08-29 12:46:33 +0100

    Scaffold App - Add NgXs for state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsStoragePlugin, NgxsStoragePluginOptions } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(
+[],
+withNgxsReduxDevtoolsPlugin(),
+withNgxsLoggerPlugin(),
+withNgxsStoragePlugin({keys: '*'} as NgxsStoragePluginOptions))]
 };
