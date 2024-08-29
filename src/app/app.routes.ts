dda46bb3bfb83ffb276be9b1b28c3c377a5bf33c
Adebayo Modupe Aisha <modupeadebayo001@gmail.com>
2024-08-29 23:31:43 +0100

    1 - Build out  Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,13 @@ export const routes: Routes = [
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
 
+  {
+    path: 'adventure',
+    loadComponent: () =>
+      import('./pages/travel-game/travel-game.component').then(
+        (m) => m.TravelGameComponent,
+      ),
+  },
   // Redirect all other routes to the home page this can be a 404 page as well
   {
     path: '**',
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, ButtonComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
