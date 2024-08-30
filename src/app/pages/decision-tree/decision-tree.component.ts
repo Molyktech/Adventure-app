a2aab1176195806ee82bdc896b0d4695cf9aaf2e
Adebayo Modupe Aisha <modupeadebayo001@gmail.com>
2024-08-30 15:22:23 +0100

    3- Build out decision tree

--- src/app/pages/decision-tree/decision-tree.component.ts
+++ src/app/pages/decision-tree/decision-tree.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  IChoice,
+  IMappedChoice,
+  IQuestion,
+} from '../../utils/models/questions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-decision-tree',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './decision-tree.component.html',
   styleUrl: './decision-tree.component.scss',
 })
-export class DecisionTreeComponent {}
+export class DecisionTreeComponent implements OnInit {
+  @Input() questions: Record<string, IQuestion> = {};
+  @Input() path: string[] = [];
+  nodes!: IQuestion;
+  questionStartKey = 'start';
+
+  ngOnInit(): void {
+    this.nodes = this.buildQuestionTree(this.questions);
+    console.log('path', this.path, this.nodes);
+  }
+
+  buildQuestionTree(questions: Record<string, IQuestion>): IQuestion {
+    const startQuestion = questions[this.questionStartKey];
+    const result: IQuestion = {
+      id: startQuestion.id,
+      text: startQuestion.text,
+      choices: this.buildChoicesTree(
+        questions,
+        startQuestion.choices,
+        startQuestion.id,
+      ),
+    };
+
+    return result;
+  }
+
+  buildChoicesTree(
+    questions: Record<string, IQuestion>,
+    choices: IChoice[],
+    selectedQuestionId: string,
+  ): IMappedChoice[] {
+    return choices
+      .map((choice) => {
+        const selectedQuestion = questions[choice.nextQuestionId];
+        if (selectedQuestion === undefined) {
+          return undefined;
+        }
+        return {
+          id: selectedQuestion.id,
+          text: selectedQuestion.text,
+          label: choice.label,
+          questionId: `${selectedQuestionId}-${choice.nextQuestionId}`,
+          nextQuestionId: choice.nextQuestionId,
+          choices: this.buildChoicesTree(
+            questions,
+            selectedQuestion.choices,
+            selectedQuestion.id,
+          ),
+        };
+      })
+      .filter((choice) => choice !== undefined);
+  }
+}
